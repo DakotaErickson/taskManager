7f930044c8b748ccb9c03bd0d13519797f45491f
Dakota Erickson <derickson29@gmail.com>
2021-01-20 11:36:48 -0600

    creating and exporting mock data for task tests. Writing a few task tests

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = require('../../src/models/user.js');
+const Task = require('../../src/models/task.js');
 
 const userOneId = new mongoose.Types.ObjectId();
 // create a user for testing routes that need authentication
@@ -14,16 +15,61 @@ const userOne = {
     }]
 }
 
-const setupDatabase = async () => {
-    // delete every user in the database before each test runs to ensure the test database is consistent
-    await User.deleteMany();
+const userTwoId = new mongoose.Types.ObjectId();
+// create a user for testing routes that need authentication
+const userTwo = {
+    _id: userTwoId,
+    name: 'Test User 2',
+    email: "test2@test.com",
+    password: "secondtestpass",
+    tokens: [{
+        token: jwt.sign({ _id: userTwoId}, process.env.JWT_SECRET)
+    }]
+}
 
-    // save the test user
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Test task one',
+    completed: false,
+    author: userOneId
+}
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Test task two',
+    completed: true,
+    author: userOneId
+}
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Test task three',
+    completed: false,
+    author: userTwo._id
+}
+
+const setupDatabase = async () => {
+    // delete every thing in the database before each test runs to ensure the test database is consistent
+    await User.deleteMany();
+    await Task.deleteMany();
+
+    // save the test users
     await new User(userOne).save();
+    await new User(userTwo).save();
+
+    // save the test tasks
+    await new Task(taskOne).save();
+    await new Task(taskTwo).save();
+    await new Task(taskThree).save();
 }
 
 module.exports = {
     userOneId,
     userOne,
+    userTwoId,
+    userTwo,
+    taskOne,
+    taskTwo,
+    taskThree,
     setupDatabase
 }
--- tests/task.test.js
+++ tests/task.test.js
@@ -1,7 +1,16 @@
 const request = require('supertest');
 const Task = require('../src/models/task.js');
 const app = require('../src/app.js');
-const { userOneId, userOne, setupDatabase} = require('./fixtures/db.js');
+const { 
+    userOneId,
+    userOne,
+    userTwoId,
+    userTwo,
+    taskOne,
+    taskTwo,
+    taskThree,
+    setupDatabase
+} = require('./fixtures/db.js');
 
 beforeEach(setupDatabase);
 
@@ -19,3 +28,22 @@ test('Should create task', async () => {
     expect(task).not.toBeNull();
     expect(task.completed).toBe(false);
 });
+
+test('Should get tasks', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200);
+
+        // assert we got tasks from test db
+        expect(response.body.length).toBe(2);
+})
+
+test('Should not delete unowned task', async () => {
+    await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404);
+})
